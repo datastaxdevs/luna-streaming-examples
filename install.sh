8e2107b884ecc1cd158126a57ba1c367b9a2226a
David Dieruf <david@dierufventures.com>
2022-12-20 11:37:20 -0500

    Fix tag signature

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 # tag::helm-repo-add[]
 helm repo add datastax-pulsar https://datastax.github.io/pulsar-helm-chart
-# end:helm-repo-add[]
+# end::helm-repo-add[]
 
 # tag::helm-install[]
 helm install \
@@ -10,7 +10,7 @@ helm install \
   --version 3.0.4 \
   my-pulsar-cluster \
   datastax-pulsar/pulsar
-# end:helm-install[]
+# end::helm-install[]
 
 # tag::wait-for-broker[]
 kubectl -n datastax-pulsar wait --for=condition=Ready pod/pulsar-broker-0
